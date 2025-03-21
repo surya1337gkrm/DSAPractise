bd9cd86bb252f183df59b48bc8c97a2705a58f40
surya1337gkrm <suryavenkatesh0@gmail.com>
2025-03-20 19:10:28 -0700

    Fizz Buzz

---
+++ fizzBuzz.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/fizz-buzz/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    // instead of using i%3 & i%5...using counters
+    const res =[]
+    let i=1,fizz=0,buzz=0
+    while(i<=n){
+        fizz++
+        buzz++
+        if(fizz===3 && buzz===5){
+            fizz=0
+            buzz=0
+            res.push("FizzBuzz")
+        }else if(fizz===3){
+            fizz=0
+            res.push('Fizz')
+        }else if(buzz===5){
+            buzz=0
+            res.push('Buzz')
+        }else{
+            res.push(`${i}`)
+        }
+        i++
+    }
+    return res
+};
