7c59bb566d4b86d715037e7902ee5d8747f643ea
surya1337gkrm <suryavenkatesh0@gmail.com>
2025-06-02 01:10:04 -0700

    Math implementations | GCD & LCM of 2 numbers

---
+++ gcdAndLcmOfTwoNumbers.js
@@ -0,0 +1,15 @@
+// GCD of two numbers
+const gcd = (a, b) => {
+  while (b) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+};
+console.log(gcd(2, 4));
+console.log(gcd(12, 18));
+
+const lcm = (a, b) => {
+  return (a * b) / gcd(a, b);
+};
+
+console.log(lcm(12,18))
