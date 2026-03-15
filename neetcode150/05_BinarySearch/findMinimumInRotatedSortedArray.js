414aa5fa2db30d1a20bdc65f1958f4fa17258cbe
surya1337gkrm <suryavenkatesh0@gmail.com>
2026-03-14 20:11:17 -0700

    Neetcode150 | Binary Search | Find minimum in rotated sorted array

---
+++ neetcode150/05_BinarySearch/findMinimumInRotatedSortedArray.js
@@ -0,0 +1,31 @@
+// L:eetcode: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  // Approach: as it is a rotated sorted array, we can use binary search to find the minimum element
+  // as numbers are rotated, we need to find which subarray is sorted and which is not sorted
+  // we can use binary search to find the minimum element in the sorted subarray
+  let l = 0,
+    r = nums.length - 1,
+    min = Infinity;
+  while (l <= r) {
+    const mid = l + Math.floor((r - l) / 2);
+    if (nums[l] <= nums[mid]) {
+      // left subarray is sorted, so min element will be the first element of the left subarray
+      min = Math.min(min, nums[l]);
+      // as we have to check for the min element, we can ignore l to mid
+      // there can be a min element in the right half
+      l = mid + 1;
+    }else{
+        // right subarray is sorted
+        // so mid is the min element
+        min = Math.min(min, nums[mid]);
+        // as we have to check for the min element, we can ignore mid to r
+        r = mid - 1;
+    }
+  }
+  return min
+};
